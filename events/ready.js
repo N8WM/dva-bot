9d61b38d123a77421fb7d59ef5ad233d3726c531
Nathan McCutchen <n8wm@icloud.com>
2025-03-31 16:44:17 -0700

    activity status

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,6 @@
 // events/ready.js
+const { ActivityType } = require('discord.js');
+
 module.exports = {
     name: 'ready',
     once: true,
@@ -50,5 +52,20 @@ module.exports = {
                 }
             }
         }
+
+        const updateStatus = () => {
+            const serverCount = client.guilds.cache.size;
+            const hubCount = client.hubChannels ? client.hubChannels.size : 0;
+            const activityMessage = `${serverCount} servers, ${hubCount} hubs`;
+
+            client.user.setPresence({
+                activities: [{ name: activityMessage, type: ActivityType.Watching }],
+                status: 'online'
+            });
+        };
+
+        // Update every 10 minutes (600,000 ms) or as desired
+        updateStatus();
+        setInterval(updateStatus, 600000);
     }
 };
